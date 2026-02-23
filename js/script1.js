b7f9bce1ad4f174042360ea3e539425c2716ca02
taniashahida-dev <taniashahida993@gmail.com>
2026-02-23 21:07:34 +0600

    Add js code for cards

--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,6 @@
-
 let Interviews = []
 let Rejecteds = []
+let currentStatus = 'All'
 
 let totalJobs = document.getElementById("total-jobs")
 let totalInterview = document.getElementById("total-interview")
@@ -10,7 +10,10 @@ const allJobList = document.getElementById('All-job-list')
 const interviewList = document.getElementById("Interview-list")
 const rejectedList = document.getElementById('Rejected-list')
 const cardContainer = document.getElementById("card-container")
-console.log(interviewList)
+
+const mainContainer = document.querySelector('main')
+const filterSection = document.getElementById('filtered-section')
+
 
 function calculateTotalcards() {
     totalJobs.innerText = cardContainer.children.length
@@ -19,3 +22,182 @@ function calculateTotalcards() {
 }
 
 calculateTotalcards()
+
+
+function btnTogle(id) {
+    allJobList.classList.add('bg-white', 'text-black')
+    interviewList.classList.add('bg-white', 'text-black')
+    rejectedList.classList.add('bg-white', 'text-black')
+
+    allJobList.classList.remove('bg-blue-700','text-white')
+    interviewList.classList.remove('bg-blue-700','text-white')
+    rejectedList.classList.remove('bg-blue-700','text-white')
+
+    const selected = document.getElementById(id)
+
+    currentStatus = id
+    selected.classList.remove('bg-white', 'text-black')
+    selected.classList.add('bg-blue-700','text-white')
+
+    if (id == 'Interview-list') {
+        cardContainer.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderInterviews()
+
+    } else if (id == 'All-job-list') {
+        cardContainer.classList.remove('hidden');
+        filterSection.classList.add('hidden')
+
+    } else if (id == 'Rejected-list') {
+        cardContainer.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderrejecteds()
+    }
+}
+
+
+mainContainer.addEventListener("click", function(event){
+
+   // ================= INTERVIEW BUTTON =================
+   if (event.target.classList.contains('interview-btn')) {
+
+        const parent = event.target.parentNode.parentNode
+
+        const companyName = parent.querySelector('.com-name').innerText
+        const position = parent.querySelector('.position').innerText
+        const sellery = parent.querySelector('.sellary').innerText
+        const description = parent.querySelector('.description').innerText
+
+        parent.querySelector('.batch').innerText = 'Interview'
+
+        const cardInformation = {
+            companyName,
+            position,
+            sellery,
+            batch: 'Interview',
+            description
+        }
+
+        const jobExist = Interviews.find(item => item.companyName == companyName)
+
+        if (!jobExist) {
+            Interviews.push(cardInformation)
+        }
+
+        // remove from rejected
+        Rejecteds = Rejecteds.filter(item => item.companyName != companyName)
+
+        if (currentStatus == 'Interview-list') {
+            renderInterviews()
+        }
+
+        calculateTotalcards()
+   }
+
+   // ================= REJECT BUTTON =================
+   else if (event.target.classList.contains('reject-btn')) {
+
+        const parent = event.target.parentNode.parentNode
+
+        const companyName = parent.querySelector('.com-name').innerText
+        const position = parent.querySelector('.position').innerText
+        const sellery = parent.querySelector('.sellary').innerText
+        const description = parent.querySelector('.description').innerText
+
+        parent.querySelector('.batch').innerText = 'Rejected'
+
+        const cardInformation = {
+            companyName,
+            position,
+            sellery,
+            batch: 'Rejected',
+            description
+        }
+
+        const jobExist = Rejecteds.find(item => item.companyName == companyName)
+
+        if (!jobExist) {
+            Rejecteds.push(cardInformation)
+        }
+
+        // remove from interview
+        Interviews = Interviews.filter(item => item.companyName != companyName)
+
+        if (currentStatus == 'Rejected-list') {
+            renderrejecteds()
+        }
+
+        calculateTotalcards()
+   }
+
+})
+
+function renderInterviews() {
+     filterSection.innerHTML=''
+
+     for(let interview of Interviews)
+     {
+         let div = document.createElement('div');
+         div.className ='mt-10 space-y-7'
+         div.innerHTML= `
+          <div class="card space-y-1.5 bg-white rounded-lg p-6">
+    <div class="font-bold flex justify-between">
+      <p class="com-name">${interview.companyName}</p>
+      <button class="delete-button text-gray-600">
+        <i class="fa-regular fa-trash-can"></i>
+      </button>
+    </div>
+    <div>
+      <p class="position text-gray-600">${interview.position}</p>
+      <p class="sellary text-gray-600">${interview.sellery}</p>
+      <p class="batch bg-gray-400 px-3.5 py-2 rounded-sm my-1.5 w-36">${interview.batch}</p>
+      <p class="description text-gray-900">
+        ${interview.description}
+      </p>
+    </div>
+    <div>
+      <button class="interview-btn border-2 border-green-600 text-green-600 py-2 px-3.5 rounded-sm">Interview</button>
+      <button class="reject-btn border-2 border-red-600 text-red-600 py-2 px-3.5 rounded-sm">Rejected</button>
+    </div>
+  </div>
+         `
+          filterSection.appendChild(div)
+
+     }
+}
+
+
+
+function renderrejecteds() {
+     filterSection.innerHTML=''
+
+     for(let list of Rejecteds)
+     {
+         let div = document.createElement('div');
+         div.className ='mt-10 space-y-7'
+         div.innerHTML= `
+          <div class="card space-y-1.5 bg-white rounded-lg p-6">
+    <div class="font-bold flex justify-between">
+      <p class="com-name">${list.companyName}</p>
+      <button class="delete-button text-gray-600">
+        <i class="fa-regular fa-trash-can"></i>
+      </button>
+    </div>
+    <div>
+      <p class="position text-gray-600">${list.position}</p>
+      <p class="sellary text-gray-600">${list.sellery}</p>
+      <p class="batch bg-gray-400 px-3.5 py-2 rounded-sm my-1.5 w-36">${list.batch}</p>
+      <p class="description text-gray-900">
+        ${list.description}
+      </p>
+    </div>
+    <div>
+      <button class="interview-btn border-2 border-green-600 text-green-600 py-2 px-3.5 rounded-sm">Interview</button>
+      <button class="reject-btn border-2 border-red-600 text-red-600 py-2 px-3.5 rounded-sm">Rejected</button>
+    </div>
+  </div>
+         `
+          filterSection.appendChild(div)
+
+     }
+}
